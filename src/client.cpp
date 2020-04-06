0c17fbdd317e51a01fb4e2cfc5a298735f5cc7cf
Mattéo Delabre <spam@delab.re>
2020-04-07 00:02:50 +0200

    Move VNC client code to separate class

---
+++ src/client.cpp
@@ -0,0 +1,172 @@
+#include "client.hpp"
+#include "log.hpp"
+#include <cerrno>
+#include <cstdlib>
+#include <chrono>
+#include <iostream>
+#include <sstream>
+#include <system_error>
+
+namespace chrono = std::chrono;
+
+/**
+ * Tag used for storing in/retrieving from the client object
+ * the structure in which updates are accumulated.
+ */
+constexpr auto update_info_tag = 1;
+
+/**
+ * Time to wait after the last update from VNC before updating the screen
+ * (in microseconds).
+ *
+ * VNC servers tend to send a lot of small updates in a short period of time.
+ * This delay allows grouping those small updates into a larger screen update.
+ */
+constexpr chrono::milliseconds update_delay{100};
+
+rfbBool create_framebuf(rfbClient*)
+{
+    // No-op: Data is written directly to the memory-mapped framebuffer
+    return true;
+}
+
+/**
+ * Register an update from the server.
+ *
+ * @param client Handle to the VNC client.
+ * @param x Left bound of the updated rectangle (in pixels).
+ * @param y Top bound of the updated rectangle (in pixels).
+ * @param w Width of the updated rectangle (in pixels).
+ * @param h Height of the updated rectangle (in pixels).
+ */
+void update_framebuf(rfbClient* client, int x, int y, int w, int h)
+{
+    // Register the region as pending update, potentially extending
+    // an existing one
+    client::update_info_struct* update_info
+        = reinterpret_cast<client::update_info_struct*>(
+                rfbClientGetClientData(
+                    client,
+                    reinterpret_cast<void*>(update_info_tag)
+                ));
+
+    log::print("VNC Update") << w << 'x' << h << '+' << x << '+' << y << '\n';
+
+    if (update_info->has_update)
+    {
+        // Merge new rectangle with existing one
+        int left_x = std::min(x, update_info->x);
+        int top_y = std::min(y, update_info->y);
+        int right_x = std::max(x + w, update_info->x + update_info->w);
+        int bottom_y = std::max(y + h, update_info->y + update_info->h);
+
+        update_info->x = left_x;
+        update_info->y = top_y;
+        update_info->w = right_x - left_x;
+        update_info->h = bottom_y - top_y;
+    }
+    else
+    {
+        update_info->x = x;
+        update_info->y = y;
+        update_info->w = w;
+        update_info->h = h;
+        update_info->has_update = 1;
+    }
+
+    update_info->last_update_time = chrono::steady_clock::now();
+}
+
+client::client(const char* ip, int port, rm::screen& rm_screen)
+: rm_screen(rm_screen), vnc_client(rfbGetClient(0, 0, 0))
+{
+    rfbClientSetClientData(
+        this->vnc_client,
+        reinterpret_cast<void*>(update_info_tag),
+        &this->update_info
+    );
+
+    free(this->vnc_client->serverHost);
+    this->vnc_client->serverHost = strdup(ip);
+    this->vnc_client->serverPort = port;
+
+    this->vnc_client->MallocFrameBuffer = create_framebuf;
+    this->vnc_client->GotFrameBufferUpdate = update_framebuf;
+
+    // Configure connection with device framebuffer settings
+    this->vnc_client->frameBuffer = this->rm_screen.get_data();
+    this->vnc_client->format.bitsPerPixel = this->rm_screen.get_bits_per_pixel();
+    this->vnc_client->format.redShift = this->rm_screen.get_red_offset();
+    this->vnc_client->format.redMax = this->rm_screen.get_red_max();
+    this->vnc_client->format.greenShift = this->rm_screen.get_green_offset();
+    this->vnc_client->format.greenMax = this->rm_screen.get_green_max();
+    this->vnc_client->format.blueShift = this->rm_screen.get_blue_offset();
+    this->vnc_client->format.blueMax = this->rm_screen.get_blue_max();
+
+    if (!rfbInitClient(this->vnc_client, nullptr, nullptr))
+    {
+        throw std::runtime_error{"Failed to initialize VNC connection"};
+    }
+
+    // Make sure the server gives us a compatible format
+    if (this->vnc_client->width < 0
+        || this->vnc_client->height < 0
+        || static_cast<unsigned int>(this->vnc_client->width)
+            != this->rm_screen.get_xres_memory()
+        || static_cast<unsigned int>(this->vnc_client->height)
+            > this->rm_screen.get_yres_memory())
+    {
+        std::stringstream msg;
+        msg << "Server uses an unsupported resolution ("
+            << this->vnc_client->width << 'x' << this->vnc_client->height
+            << "). This client can only cope with a screen width of exactly "
+            << this->rm_screen.get_xres_memory() << " pixels and a screen "
+            "height no larger than " << this->rm_screen.get_yres_memory()
+            << " pixels";
+        throw std::runtime_error{msg.str()};
+    }
+}
+
+client::~client()
+{
+    rfbClientCleanup(this->vnc_client);
+}
+
+void client::start()
+{
+    while (true)
+    {
+        int result = WaitForMessage(
+            this->vnc_client,
+            /* timeout = */
+            chrono::duration_cast<chrono::microseconds>(update_delay).count()
+        );
+
+        if (result < 0)
+        {
+            throw std::system_error(
+                errno,
+                std::generic_category(),
+                "(main) Wait for message"
+            );
+        }
+
+        if (result > 0)
+        {
+            if (!HandleRFBServerMessage(this->vnc_client))
+            {
+                break;
+            }
+        }
+
+        if (this->update_info.has_update && this->update_info.last_update_time
+                + update_delay < chrono::steady_clock::now())
+        {
+            this->update_info.has_update = 0;
+            this->rm_screen.update(
+                this->update_info.x, this->update_info.y,
+                this->update_info.w, this->update_info.h
+            );
+        }
+    }
+}
---
+++ src/client.hpp
@@ -0,0 +1,63 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+#include "screen.hpp"
+#include <chrono>
+#include <rfb/rfbclient.h>
+// IWYU pragma: no_include <rfb/rfbproto.h>
+
+/**
+ * VNC client for the reMarkable tablet.
+ */
+class client
+{
+public:
+    /**
+     * Create a VNC client.
+     *
+     * @param ip IP address of the VNC server to connect to.
+     * @param port Port of the VNC server to connect to.
+     * @param rm_screen Screen to update with data from the VNC server.
+     */
+    client(const char* ip, int port, rm::screen& rm_screen);
+
+    /**
+     * Start the client event loop.
+     */
+    void start();
+
+    ~client();
+
+    /** Accumulator for updates received from the VNC server. */
+    struct update_info_struct
+    {
+        /** Left bound of the overall updated rectangle (in pixels). */
+        int x;
+
+        /** Top bound of the overall updated rectangle (in pixels). */
+        int y;
+
+        /** Width of the overall updated rectangle (in pixels). */
+        int w;
+
+        /** Height of the overall updated rectangle (in pixels). */
+        int h;
+
+        /** Whether at least one update has been registered. */
+        short has_update;
+
+        /** Last time an update was registered (in microseconds). */
+        std::chrono::steady_clock::time_point last_update_time;
+    };
+
+private:
+    /** VNC connection. */
+    rfbClient* vnc_client;
+
+    /** reMarkable screen. */
+    rm::screen& rm_screen;
+
+    update_info_struct update_info;
+}; // class client
+
+#endif // CLIENT_HPP
